5cccb3f83603beda5fff1ddf165f00e73e9abfc7
Thomas Mauro <thom_mauro@outlook.com>
2025-07-24 15:41:34 -0300

    adicionado menu de relatorios

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,7 @@
                         <!-- Dropdown ajustado para integrar com o layout -->
                         <div class="flex items-center">
 
+                            {{-- Ordem de Serviço --}}
                             <x-dropdown>
                                 <x-slot name="trigger">
                                     <button class="inline-flex items-center px-3 py-2 text-sm leading-4 font-medium rounded-md text-gray-500 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -37,6 +38,7 @@
                                 </x-slot>
                             </x-dropdown>
 
+                            {{--  Clientes --}}
                             <x-dropdown>
                                 <x-slot name="trigger">
                                     <button class="inline-flex items-center px-3 py-2 text-sm leading-4 font-medium rounded-md text-gray-500 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -55,6 +57,29 @@
                                     </x-dropdown-link>
                                     <x-dropdown-link :href="route('customer.index')">
                                         Ver clientes
+                                    </x-dropdown-link>
+                                </x-slot>
+                            </x-dropdown>
+
+                            {{-- Relatórios --}}
+                            <x-dropdown>
+                                <x-slot name="trigger">
+                                    <button class="inline-flex items-center px-3 py-2 text-sm leading-4 font-medium rounded-md text-gray-500 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                        <div>Relatórios</div>
+                                        <div class="ms-1">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    <x-dropdown-link :href="route('reports.client')">
+                                        O.S por cliente
+                                    </x-dropdown-link>
+                                    <x-dropdown-link :href="route('reports.month')">
+                                        Vendas por mês
                                     </x-dropdown-link>
                                 </x-slot>
                             </x-dropdown>
